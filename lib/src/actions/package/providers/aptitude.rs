01dfee16d6ba53c4888a7cb39b2613c62a4def06
Peter Brunner <peter@lugoues.net>
2025-08-23 20:58:15 +0000

    write apt source list manually instead of using apt-add-repository which is not supported in debian >= 13

--- lib/src/actions/package/providers/aptitude.rs
+++ lib/src/actions/package/providers/aptitude.rs
@@ -5,7 +5,6 @@ use crate::contexts::Contexts;
 use crate::steps::Step;
 use crate::utilities;
 use serde::{Deserialize, Serialize};
-use sha256::digest;
 use tracing::warn;
 use which::which;
 
@@ -27,10 +26,10 @@ impl PackageProvider for Aptitude {
     }
 
     fn available(&self) -> bool {
-        match which("apt-add-repository") {
+        match which("apt") {
             Ok(_) => true,
             Err(_) => {
-                warn!(message = "apt-add-repository not available");
+                warn!(message = "apt not available");
                 false
             }
         }
@@ -46,7 +45,6 @@ impl PackageProvider for Aptitude {
                 arguments: vec![
                     String::from("install"),
                     String::from("--yes"),
-                    String::from("software-properties-common"),
                     String::from("curl"),
                     String::from("gpg"),
                 ],
@@ -66,75 +64,10 @@ impl PackageProvider for Aptitude {
 
     fn add_repository(
         &self,
-        repository: &PackageRepository,
-        contexts: &Contexts,
+        _: &PackageRepository,
+        _contexts: &Contexts,
     ) -> anyhow::Result<Vec<Step>> {
-        let mut steps: Vec<Step> = vec![];
-
-        let mut signed_by = String::from("");
-
-        let privilege_provider =
-            utilities::get_privilege_provider(&contexts).unwrap_or_else(|| "sudo".to_string());
-
-        if repository.key.is_some() {
-            // .unwrap() is safe here because we checked for key.is_some() above
-            let key = repository.clone().key.unwrap();
-
-            let key_name = key.name.unwrap_or_else(|| digest(&*key.url));
-            let key_path = format!("/usr/share/keyrings/{}.asc", key_name);
-
-            signed_by = format!("signed-by={}", key_path);
-
-            steps.push(Step {
-                atom: Box::new(Exec {
-                    command: String::from("curl"),
-                    arguments: vec![String::from("-o"), key_path, key.url],
-                    environment: self.env(),
-                    privileged: true,
-                    privilege_provider: privilege_provider.clone(),
-                    ..Default::default()
-                }),
-                initializers: vec![],
-                finalizers: vec![],
-            });
-        }
-
-        //sudo apt-add-repository "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/<myrepository>-archive-keyring.gpg] https://repository.example.com/debian/ $(lsb_release -cs) stable main "
-        steps.extend(vec![
-            Step {
-                atom: Box::new(Exec {
-                    command: String::from("apt-add-repository"),
-                    arguments: vec![
-                        String::from("-y"),
-                        format!(
-                            "deb [arch=$(dpkg --print-architecture) {}] {}",
-                            signed_by,
-                            repository.name.clone()
-                        ),
-                    ],
-                    environment: self.env(),
-                    privileged: true,
-                    privilege_provider: privilege_provider.clone(),
-                    ..Default::default()
-                }),
-                initializers: vec![],
-                finalizers: vec![],
-            },
-            Step {
-                atom: Box::new(Exec {
-                    command: String::from("apt"),
-                    arguments: vec![String::from("update")],
-                    environment: self.env(),
-                    privileged: true,
-                    privilege_provider: privilege_provider.clone(),
-                    ..Default::default()
-                }),
-                initializers: vec![],
-                finalizers: vec![],
-            },
-        ]);
-
-        Ok(steps)
+        Ok(vec![])
     }
 
     fn query(&self, package: &PackageVariant) -> anyhow::Result<Vec<String>> {
